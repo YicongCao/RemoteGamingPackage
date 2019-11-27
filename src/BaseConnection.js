f7ee30854551a3a7ea38508a306cc0fd1a92ec87
yicongcao <yicongcao@gmail.com>
2019-11-27 23:28:15 +0800

    实现基础连接的服务端逻辑，抽离重复代码

--- src/BaseConnection.js
+++ src/BaseConnection.js
@@ -10,11 +10,11 @@ class BaseConnection {
         this.connid = invalidConnID
         this.status = Enums.Status.INVALID
         // callbacks
-        this.onconnectreq = null    // server
-        this.onconnected = null     // client
-        this.onerror = null
-        this.onhung = null
-        this.ondata = null
+        // this.onconnectreq = null    // server
+        // this.onconnected = null     // client
+        // this.onerror = null
+        // this.onhung = null
+        // this.ondata = null
     }
 
     /**
@@ -22,9 +22,9 @@ class BaseConnection {
      * @param {string} url target url, e.g. ws://localhost:7667
      */
     connect(url) {
-        this.conn = new WebSocket(url)
-        this.conn.binaryType = 'arraybuffer'
-        this.status = Enums.Status.UNCONNECTED
+        if (!this._setconn(new WebSocket(url))) {
+            return
+        }
 
         this.conn.on('open', function _onopen() {
             this.status = Enums.Status.UNCONFIRMED
@@ -35,57 +35,112 @@ class BaseConnection {
             this._sendraw(rgpBaseLayerBuffer)
         }.bind(this))
 
-        this.conn.on('message', function _onmsg(e) {
-            var rgpDataBuffer = e.data
-            if (!rgpDataBuffer instanceof ArrayBuffer) {
-                console.error("data received not arraybuffer")
-                return
-            }
-            console.log("received", e.data.byteLength, "byte long data")
-            // parse all layers
-            var rgpDataPackets = Protocol.ProtocolSerializer.UnpackAllAsMap(e.data)
-            // process data on conditions
-            switch(this.status) {
-                case Enums.Status.UNCONFIRMED:
-                    if (rgpDataPackets[Enums.ProtoName.BASE_LAYER].command == 
-                        Enums.BaseLayerCommand.SERVER_CONFIRM) {
-                            this.status = Enums.Status.CONNECTED
-                            this.connid = rgpDataPackets[Enums.ProtoName.BASE_LAYER].connectionID
-                            console.log("rgp conn confirmed, connid:", this.connid)
-                        }
-                    else if (rgpDataPackets[Enums.ProtoName.BASE_LAYER].command == 
-                        Enums.BaseLayerCommand.SERVER_REJECT) {
-                            this.status = Enums.Status.REJECTED
-                            console.error("rgp conn denied by server")
-                        }
-                    else {
+        this.conn.on('message', function _onclientmsg(e) {
+            var callbackMap = new Map()
+            callbackMap[Enums.Status.UNCONFIRMED] = (rgpPacketMap) => {
+                var basePacket = rgpPacketMap[Enums.ProtoName.BASE_LAYER]
+                switch (basePacket.command) {
+                    case Enums.BaseLayerCommand.SERVER_CONFIRM:
+                        this.status = Enums.Status.CONNECTED
+                        this.connid = basePacket.connectionID
+                        console.log("rgp conn confirmed, connid:", this.connid)
+                        return Enums.ErrorCodes.SUCCEEDED
+                    case Enums.BaseLayerCommand.SERVER_REJECT:
+                        this.status = Enums.Status.REJECTED
+                        console.error("rgp conn denied by server")
+                        return Enums.ErrorCodes.SERVER_REJECTED
+                    default:
                         this.status = Enums.Status.HUNG
                         console.error("illegal command when conn unconfirmed")
-                    }
-                    break
-                default:
-                    console.error("match no callbacks")
-                    break
+                        return Enums.ErrorCodes.UNSUPPORTED_COMMAND
+                }
             }
+            this._process(e, callbackMap)
         }.bind(this))
     }
 
-    setconn(connPara) {
-        if (connPara instanceof WebSocket) {
-            console.error("conn para not instance of ws:", connPara)
+    /**
+     * Handle client connect action
+     * @param {WebSocket} ws websocket instance created by server
+     * @param {int} clientID conn id that identifies different clients
+     */
+    onconnectreq(ws, clientID) {
+        if (!this._setconn(ws, clientID)) {
             return
         }
-        this.conn = connPara
+
+        this.conn.on('message', function _onservermsg(e) {
+            var callbackMap = new Map()
+            callbackMap[Enums.Status.UNCONFIRMED] = (rgpPacketMap) => {
+                var basePacket = rgpPacketMap[Enums.ProtoName.BASE_LAYER]
+                switch (basePacket.command) {
+                    case Enums.BaseLayerCommand.CLIENT_ACQUIRE:
+                        var accept = this.connid && this.connid != invalidConnID
+                        var resp = null
+                        if (accept) {
+                            this.status = Enums.Status.CONNECTED
+                            var rgpBaseLayerPacket = new Protocol.BaseLayerPacket(Enums.Proto.NONE_LAYER, 
+                                Enums.BaseLayerCommand.SERVER_CONFIRM, clientID)
+                            var resp = Protocol.ProtocolSerializer.PackBaseLayer(rgpBaseLayerPacket)
+                            console.log("rgp conn confirmed, connid:", this.connid)
+                        } else {
+                            this.status = Enums.Status.REJECTED
+                            var rgpBaseLayerPacket = new Protocol.BaseLayerPacket(Enums.Proto.NONE_LAYER, 
+                                Enums.BaseLayerCommand.SERVER_REJECT, clientID)
+                            var resp = Protocol.ProtocolSerializer.PackBaseLayer(rgpBaseLayerPacket)
+                            console.error("rgp conn denied by server")
+                        }
+                        this._sendraw(resp)
+                        return Enums.ErrorCodes.SUCCEEDED
+                    default:
+                        return Enums.ErrorCodes.UNSUPPORTED_COMMAND
+                }
+            }
+            this._process(e, callbackMap)
+        }.bind(this))
+    }
+
+    _setconn(ws, connidPara = invalidConnID) {
+        if (ws.__proto__ != WebSocket.prototype) {
+            console.error("conn para not instance of ws:", ws)
+            return false
+        }
+        this.conn = ws
+        this.conn.binaryType = 'arraybuffer'
+        this.connid = connidPara
+        this.status = Enums.Status.UNCONFIRMED
+        return true
     }
 
     _sendraw(arrayBuffer) {
-        if (!arrayBuffer instanceof ArrayBuffer) {
+        if (!(arrayBuffer instanceof ArrayBuffer)) {
             console.error("data to send not arraybuffer")
             return
         }
         var length = arrayBuffer.byteLength
         console.log("sending", length, "byte long data")
         this.conn.send(arrayBuffer)
+    }
+
+    _process(data, callbackMap) {
+        if (!(data instanceof ArrayBuffer)) {
+            console.error("data received not arraybuffer")
+            return
+        }
+        console.log("received", data.byteLength, "byte long data")
+        if (!(callbackMap instanceof Map)) {
+            console.error("illegal callbackmap")
+            return
+        }
+        if (!callbackMap[this.status]) {
+            console.error("callback unimplenmented for status", this.status)
+            return
+        }
+        // parse all layers
+        var rgpDataPackets = Protocol.ProtocolSerializer.UnpackAllAsMap(data)
+        // process data on conditions
+        var callbackRet = callbackMap[this.status](rgpDataPackets)
+        console.log("handled with ret:", callbackRet)
     }
 }
 
